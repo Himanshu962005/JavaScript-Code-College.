3c6b6e56fa203099f05f512b7fd45db7447b11c9
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-05-28 15:17:38 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main2.js
@@ -0,0 +1,16 @@
+alert("Hello World");
+console.log("Code is Running...");
+console.log("Code is also Running...");
+console.log("Code is Looking Like a wow...");
+var a = prompt("Enter Your Number");
+var isTrue = confirm(
+  "Are You Sure You Want to Leave this Page and Blast Your Computer.",
+);
+if (isTrue) {
+  console.log("Computer is Blasting");
+} else {
+  console.log("Computer is not Blasting");
+}
+console.log("Your Number is : " + a);
+document.title = "Hey I am Good";
+// document.body.style.backgroundColor = "red".

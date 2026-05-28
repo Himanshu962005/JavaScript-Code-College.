9583f750c903dcc172e22437fe7228cabd9be89d
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-05-28 18:57:13 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main3.js
@@ -0,0 +1,34 @@
+console.log("Hey, JavaScript Variables.");
+var a = 5;
+// a = a + 1
+let b = 6;
+let c = "Himanshu";
+let _a = "Divya";
+// var 55a = "Prashant"; // Not Allowed.
+// console.log(a +  b + 8);
+// console.log(typeof a, typeof b, typeof c);
+{
+  // let a = 66;
+  console.log(a);
+}
+console.log(a);
+// const a1 = 6;
+// a1 = a1 + 1; // Not Allowed Because A1 is Constant.
+let x = "Himanshu Bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+let o = {
+  name: "Himanshu",
+  "job code": 5600,
+  is_handsome: true,
+};
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);

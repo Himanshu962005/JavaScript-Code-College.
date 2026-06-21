4fd08e0efc2cfb056bb67627a6f7bdb9698af7ef
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-06-21 10:07:20 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main6.js
@@ -0,0 +1,23 @@
+function Nice(name) {
+  console.log("Hey " + name + " You are Nice!");
+  console.log("Hey " + name + " You are Good!");
+  console.log("Hey " + name + " Your T-shirt is Nice!");
+  console.log("Hey " + name + " Your Course is Good Too!");
+}
+function sum(a, b, c = 3) {
+  // console.log(a + b);
+  console.log(a, b, c);
+  return a + b + c;
+}
+result1 = sum(3, 2);
+result2 = sum(7, 5);
+result3 = sum(3, 13, 1);
+console.log("The Sum of these Numbers is :", result1);
+console.log("The Sum of these Numbers is :", result2);
+console.log("The Sum of these Numbers is :", result3);
+const func1 = (x) => {
+  console.log("I am an Arrow Function :", x);
+};
+func1(34);
+func1(66);
+func1(84);

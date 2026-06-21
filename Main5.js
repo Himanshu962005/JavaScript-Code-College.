d2fe4059adeee0dea9b9d125d9b18ab7a8fab1f5
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-06-21 09:49:18 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main5.js
@@ -0,0 +1,29 @@
+console.log("I am a Loops.");
+let a = 1;
+// console.log(a);
+// console.log(a+1);
+// console.log(a+2);
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+// let obj = {
+//     name: "Himanshu",
+//     role: "Programmer",
+//     company: "Google"
+// };
+// for (const key in obj) {
+//         console.log(key);
+// };
+// for (const c of "Himanshu") {
+//     console.log(c);
+// };
+// let i = 0;
+// while (i<60000) {
+//     console.log(i)
+//     i++;
+// }
+let i = 10;
+do {
+  console.log(i);
+  i++;
+} while (i < 6);

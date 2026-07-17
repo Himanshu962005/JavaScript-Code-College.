0807064c1bebae2f3b6b14dc3e04689248c01e35
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-07-17 11:55:15 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main18.js
@@ -0,0 +1,17 @@
+// Exercise 11 - Calculate the Factorial.
+// Write a Program to Calculate Factorial of a Number Using Reduce and Using for Loops 6! = 6*5*4*3*2*1.
+let a = 7;
+function factorial(number) {
+    let arr = Array.from(Array(number + 1).keys());
+    let c = arr.slice(1,).reduce((a, b) => a * b);
+    return c;
+};
+function facFor(number) {
+    let fac = 1;
+    for (let index = 1; index <= number; index++) {
+        fac = fac * index;
+    };
+    return fac;
+};
+console.log(factorial(a));
+console.log(facFor(a));

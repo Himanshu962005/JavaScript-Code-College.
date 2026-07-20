40f4f53066f8824d481d2b37ae0308e3ab9bc483
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-07-20 17:12:26 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main36.js
@@ -0,0 +1,7 @@
+// Q.Write a Program to Sum and Multiply Two Numbers using JavaScript.
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+let multiply = num1 * num2;
+console.log("Sum =", sum);
+console.log("Multiplication =", multiply);
